f81ae6a8df8168443c482cf666766d9c2ee19120
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-21 23:05:42 -0230

    add: middle1

--- partials/front-page/middle1.php
+++ partials/front-page/middle1.php
@@ -1,9 +1,12 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box lg:py-40 lg:pb-20 py-30">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="md:flex justify-between gap-10 w-full text-center md:text-left">
+            <h2 class="lg:text-h2 md:text-h3 text-h5 whitespace-nowrap">About Me</h2>
+            
+            <div class="md:mt-1 md:max-w-[733px]">
+                <p class="md:text-paragraph-large text-paragraph-big opacity-87">Hi, my name is James and I'm a designer from San Francisco. I have over 10 years experience in leading teams and shipping complex digital products.</p>
+                <a href="#" class="text-primary text-lg font-medium hover:underline mx-auto md:mx-0 block mt-8 max-w-max tracking-[-0.36px]">More About Me</a>
+            </div>
         </div>
     </div>
 </div>
