0ce952d5b49b3c54ffb9baed165dcde17c179349
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-22 15:03:52 -0230

    add: button to bottom4

--- partials/front-page/bottom4.php
+++ partials/front-page/bottom4.php
@@ -1,11 +1,11 @@
 <div class="main-box">
-    <div class="wrapper border-t border-t-white/17 py-20">
+    <div class="wrapper border-y border-y-white/17 py-20">
         <div class="w-full text-center lg:text-left">
             <span class="text-cap-1 opacity-87 uppercase">My Recent Work</span>
-            <h2 class="lg:mt-6 mt-2 lg:text-h2 md:text-h4 text-h5 opacity-85">I love taking Design from vision to reality.</h2>
+            <h2 class="lg:max-w-[446px] lg:mt-6 mt-2 lg:text-h2 md:text-h4 text-h5 opacity-85">I love taking Design from vision to reality.</h2>
         </div>
 
-        <div class="lg:mt-20 mt-10 columns-2  gap-2 lg:gap-x-8">
+        <div class="lg:mt-20 mt-10 columns-2 gap-2 lg:gap-x-8">
           <div class="group max-w-max- mx-auto mb-2 lg:mb-11 relative cursor-pointer">
             <div class="group-hover:flex hidden absolute top-0 left-0 w-full h-full bg-black/64 text-h4 justify-center items-center">
               <span>View Project</span>
@@ -34,5 +34,7 @@
             <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom4-img4.jpg" alt="portfolio"/>
           </div>
         </div>
+
+        <a href="#" class="button-primary mx-auto lg:mt-16 mt-10 max-w-max block">View all projects</a>
     </div>
 </div>
