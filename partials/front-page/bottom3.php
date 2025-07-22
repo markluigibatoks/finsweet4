0d3d81391326eec8cba5bb16c9a6ee4acdc9a844
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-22 14:32:56 -0230

    add:  bottom3

---
+++ partials/front-page/bottom3.php
@@ -0,0 +1,52 @@
+<div class="main-box lg:pt-40 lg:pb-20 pt-30 pb-20">
+    <div class="wrapper">
+        <div class="lg:flex justify-between gap-10 w-full text-center lg:text-left">
+            <div class="xl:max-w-[421px] lg:max-w-[320px]">
+              <h2 class="xl:text-h2 lg:text-h3 text-h5">Worked with top company all over world</h2>
+            </div>
+            
+            <ul class="mt-10 lg:mt-0 grid w-full gap-17 mx-auto md:max-w-[820px] grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-between">
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img1.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img2.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img3.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img4.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img5.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img6.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img7.png" alt="logo"/>
+                  </div>
+                </li>
+                <li>
+                  <div class="max-w-max- mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img8.png" alt="logo"/>
+                  </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
