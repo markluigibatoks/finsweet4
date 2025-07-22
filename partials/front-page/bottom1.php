8293c86789f5ff4d58041c76cfc38a15e5cf1ca7
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-22 00:38:19 -0230

    add: bottom1

--- partials/front-page/bottom1.php
+++ partials/front-page/bottom1.php
@@ -1,9 +1,53 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box lg:py-40 py-30">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="lg:flex justify-between gap-10 w-full text-center lg:text-left">
+            <h2 class="lg:text-h2 md:text-h3 text-h5 whitespace-nowrap">Services</h2>
+            
+            <div class="mt-10 lg:mt-1 md:max-w-[759px]">
+                <ul class="md:columns-2 lg:gap-14">
+                    <li class="mb-10">
+                        <div class="flex gap-4 md:gap-8 items-center">
+                            <div class="w-8 h-8 rounded-full bg-white/9 flex items-center justify-center">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
+                            </div>
+                            <span class="md:text-h3 text-h5 whitespace-nowrap">Product Design</span>
+                        </div>
+                    </li>
+                    <li class="mb-10">
+                        <div class="flex gap-4 md:gap-8 items-center">
+                            <div class="w-8 h-8 rounded-full bg-white/9 flex items-center justify-center">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
+                            </div>
+                            <span class="md:text-h3 text-h5 whitespace-nowrap">Branding</span>
+                        </div>
+                    </li>
+                    <li class="mb-10">
+                        <div class="flex gap-4 md:gap-8 items-center">
+                            <div class="w-8 h-8 rounded-full bg-white/9 flex items-center justify-center">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
+                            </div>
+                            <span class="md:text-h3 text-h5 whitespace-nowrap">Visual & UI Design</span>
+                        </div>
+                    </li>
+                    <li class="mb-10">
+                        <div class="flex gap-4 md:gap-8 items-center">
+                            <div class="w-8 h-8 rounded-full bg-white/9 flex items-center justify-center">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
+                            </div>
+                            <span class="md:text-h3 text-h5 whitespace-nowrap">Webflow Devlopement</span>
+                        </div>
+                    </li>
+                    <li class="mb-10">
+                        <div class="flex gap-4 md:gap-8 items-center">
+                            <div class="w-8 h-8 rounded-full bg-white/9 flex items-center justify-center">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
+                            </div>
+                            <span class="md:text-h3 text-h5 whitespace-nowrap">Php Development</span>
+                        </div>
+                    </li>
+                    
+                </ul>
+            </div>
         </div>
     </div>
 </div>
