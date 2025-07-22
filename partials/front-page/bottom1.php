741f269898d46b13fd925e613003015f0cd17c94
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-22 17:38:22 -0230

    refactor: adjust the font size

--- partials/front-page/bottom1.php
+++ partials/front-page/bottom1.php
@@ -3,13 +3,13 @@
         <div class="lg:flex justify-between gap-10 w-full text-center lg:text-left">
             <h2 class="lg:text-h2 md:text-h3 text-h5 whitespace-nowrap">Services</h2>
             
-            <ul class="mt-10 lg:mt-1 md:max-w-[820px] w-full grid grid-cols-1 #md:grid-cols-2 gap-10">
+            <ul class="mt-10 lg:mt-1 md:max-w-[820px] w-full grid grid-cols-1 md:grid-cols-2 gap-10">
                 <li>
                     <div class="flex gap-4 md:gap-8 items-center">
                         <div class="w-8 h-8 min-w-8 min-h-8 rounded-full bg-white/9 flex items-center justify-center">
                         <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
                         </div>
-                        <span class="md:text-h3 text-h5 whitespace-nowrap">Product Design</span>
+                        <span class="lg:text-h3 md:text-h4 text-h5 whitespace-nowrap">Product Design</span>
                     </div>
                 </li>
                 <li>
@@ -17,7 +17,7 @@
                         <div class="w-8 h-8 min-w-8 min-h-8 rounded-full bg-white/9 flex items-center justify-center">
                         <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
                         </div>
-                        <span class="md:text-h3 text-h5 whitespace-nowrap">Branding</span>
+                        <span class="lg:text-h3 md:text-h4 text-h5 whitespace-nowrap">Branding</span>
                     </div>
                 </li>
                 <li>
@@ -25,7 +25,7 @@
                         <div class="w-8 h-8 min-w-8 min-h-8 rounded-full bg-white/9 flex items-center justify-center">
                         <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
                         </div>
-                        <span class="md:text-h3 text-h5 whitespace-nowrap">Visual & UI Design</span>
+                        <span class="lg:text-h3 md:text-h4 text-h5 whitespace-nowrap">Visual & UI Design</span>
                     </div>
                 </li>
                 <li>
@@ -33,7 +33,7 @@
                         <div class="w-8 h-8 min-w-8 min-h-8 rounded-full bg-white/9 flex items-center justify-center">
                         <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
                         </div>
-                        <span class="md:text-h3 text-h5 whitespace-nowrap">Webflow Development</span>
+                        <span class="lg:text-h3 md:text-h4 text-h5 whitespace-nowrap">Webflow Development</span>
                     </div>
                 </li>
                 <li>
@@ -41,7 +41,7 @@
                         <div class="w-8 h-8 min-w-8 min-h-8 rounded-full bg-white/9 flex items-center justify-center">
                         <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom1-icon1.svg'); ?>
                         </div>
-                        <span class="md:text-h3 text-h5 whitespace-nowrap">Php Development</span>
+                        <span class="lg:text-h3 md:text-h4 text-h5 whitespace-nowrap">Php Development</span>
                     </div>
                 </li>
                 
